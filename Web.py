4f16fa619e62711b5981cf4b10b4a9eee9fc72d2
CHarliE <159606994+CHarliE-1509@users.noreply.github.com>
2024-04-14 23:41:05 +0530

    Update Web.py
    Initialising NSEI as default value

--- Web.py
+++ Web.py
@@ -36,7 +36,7 @@ with Home:
 #Making of Dashboard 
 with Dashboard:
     st.title("Stock Dashboard")
-    ticker=st.sidebar.text_input('Ticker')
+    ticker=st.sidebar.text_input('Ticker', value='^NSEI')
     start_date=st.sidebar.date_input('Start Date')
     end_date=st.sidebar.date_input('End Date')
 
